dc0c931b3ee1c7b1419e0c30173c88d3bf5a782d
MarcoMauricio <marcoflip.fm@gmail.com>
2014-05-15 15:40:11 +0100

    Script de configuração da associação rota-controlador-view

---
+++ Presentation/EggOn.Web.UI/Areas/Context/Scripts/config.js
@@ -0,0 +1,19 @@
+﻿angular.module('EggOn.Context', ['EggOn.Routes'])
+    .config(['$stateProvider', function ($stateProvider) {
+
+        $stateProvider
+
+            .state('manager.context', {
+                url: '/context',
+                abstract: true,
+                templateUrl: '/Areas/Context/Views/Index.html',
+                controller: 'ContextController'
+            })
+
+                .state('manager.context.dashboard', {
+                    url: '',
+                    templateUrl: '/Areas/Context/Views/Dashboard.html'
+                })
+
+
+    }]);
