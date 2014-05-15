f5593783d43907b333df97e539f8edeec9274cb1
MarcoMauricio <marcoflip.fm@gmail.com>
2014-05-15 14:21:56 +0100

    Controller to answer the requests for the context module

---
+++ Modules/EggOn.Context/Controllers/DocumentController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Net;
+using AutoMapper;
+using FlowOptions.EggOn.Base.Controllers;
+using FlowOptions.EggOn.Context.ViewModels;
+using System.Web.Http;
+using System.Net.Http;
+using FlowOptions.EggOn.Context.Models;
+using FlowOptions.EggOn.ModuleCore;
+using System.Web;
+
+namespace EggOn.Context.Controllers
+{
+    public class DocumentController : EggOnApiController
+    {
+        [Route("Context"), HttpPost]
+        public DocumentDto CreateDocument()
+        {
+            DocumentDto doc = new DocumentDto();
+            doc.Text = "Hello";
+            doc.SummarizedText = "Test";
+            return doc;
+        }
+ 
+    }
+}
