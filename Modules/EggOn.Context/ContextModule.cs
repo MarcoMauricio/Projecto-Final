930e100d870bdf009d66f3cff98ae5d9e6c3e784
MarcoMauricio <marcoflip.fm@gmail.com>
2014-05-15 14:20:13 +0100

    Added Module to the Context library

---
+++ Modules/EggOn.Context/ContextModule.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FlowOptions.EggOn.DataHost;
+using FlowOptions.EggOn.Context.Models;
+using FlowOptions.EggOn.Logging;
+using FlowOptions.EggOn.ModuleCore;
+
+namespace EggOn.Web.Service.Areas.Context
+{
+    public class ContextModule : IEggOnModule
+    {
+        public void Setup()
+        {
+            Logger.Debug("Application is registering the Context Area.");
+        }
+    }
+}
