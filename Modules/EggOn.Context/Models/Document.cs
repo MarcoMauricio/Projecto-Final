41e151d8e324a738d7cc097721416322533fc180
MarcoMauricio <marcoflip.fm@gmail.com>
2014-05-15 14:21:02 +0100

    Added a model and a view model to represent the tables of the data to be stored

---
+++ Modules/EggOn.Context/Models/Document.cs
@@ -0,0 +1,25 @@
+﻿using FlowOptions.EggOn.Base;
+using FlowOptions.EggOn.Base.Models;
+using FlowOptions.EggOn.DataHost;
+using FlowOptions.EggOn.ModuleCore;
+using System;
+using System.Collections.Generic;
+
+namespace FlowOptions.EggOn.Context.Models
+{
+    [TableName("[EggOn].[DocumentDocument]"), PrimaryKey("Id", autoIncrement = false)]
+
+    public class Document
+    {
+        [DefaultValue("newsequentialid()")]
+        public Guid Id { get; set; }
+
+        public string Text { get; set; }
+
+        public string SummarizedText { get; set; }
+  
+        public DateTime? SubmitDateTime { get; set; }
+
+        public List<string> ExtractedEntities { get; set; }
+    }
+}
---
+++ Modules/EggOn.Context/ViewModels/DocumentDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace FlowOptions.EggOn.Context.ViewModels
+{
+    [DataContract(Name = "Document", Namespace = "http://www.eggon.pt/EggOn.Service.Models")]
+
+    public class DocumentDto
+    {
+       
+        [DataMember(Order = 1)]
+        public Guid Id { get; set; }
+
+        [DataMember(Order = 2)]
+        public string Text { get; set; }
+
+        [DataMember(Order = 3)]
+        public string SummarizedText { get; set; }
+
+
+        [DataMember(Order = 4)]
+        public List<string> Entities { get; set; }
+    }
+}
