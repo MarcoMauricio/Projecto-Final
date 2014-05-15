392f00b7a2aebd54f1080e9875588189207ef418
MarcoMauricio <marcoflip.fm@gmail.com>
2014-05-15 15:40:37 +0100

    Comentado para não dar erro 404

--- Utilities/EggOn.Logging/Logger.cs
+++ Utilities/EggOn.Logging/Logger.cs
@@ -8,41 +8,41 @@ namespace FlowOptions.EggOn.Logging
 {
     public static class Logger
     {
-        public static NLog.Logger _logger = NLog.LogManager.GetLogger("EggOn");
+       // public static NLog.Logger _logger = NLog.LogManager.GetLogger("EggOn");
 
         public static void Log(LogLevel level, params object[] message)
         {
-            _logger.Log(NLog.LogLevel.FromOrdinal((int)level), String.Join(" ", message));
+      //      _logger.Log(NLog.LogLevel.FromOrdinal((int)level), String.Join(" ", message));
         }
 
         public static void Trace(params object[] message)
         {
-            _logger.Trace(String.Join(" ", message));
+      //      _logger.Trace(String.Join(" ", message));
         }
 
         public static void Debug(params object[] message)
         {
-            _logger.Debug(String.Join(" ", message));
+      //      _logger.Debug(String.Join(" ", message));
         }
 
         public static void Info(params object[] message)
         {
-            _logger.Info(String.Join(" ", message));
+       //     _logger.Info(String.Join(" ", message));
         }
 
         public static void Warn(params object[] message)
         {
-            _logger.Warn(String.Join(" ", message));
+        //    _logger.Warn(String.Join(" ", message));
         }
 
         public static void Error(params object[] message)
         {
-            _logger.Error(String.Join(" ", message));
+        //    _logger.Error(String.Join(" ", message));
         }
 
         public static void Fatal(params object[] message)
         {
-            _logger.Fatal(String.Join(" ", message));
+        //    _logger.Fatal(String.Join(" ", message));
         }
     }
 
