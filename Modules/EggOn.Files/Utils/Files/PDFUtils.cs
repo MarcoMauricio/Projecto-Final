a66443108c8005305ea5ba9f3baad01bc0e92207
MarcoMauricio <marcoflip.fm@gmail.com>
2014-05-23 14:47:23 +0100

    Leitura de Texto de um PDF

---
+++ Modules/EggOn.Files/Utils/Files/PDFUtils.cs
@@ -0,0 +1,26 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EggOn.Files.Context
+{
+    public class PDFUtils
+    {
+        public static string GetPDFText(String pdfPath)
+        {
+            PdfReader reader = new PdfReader(pdfPath);
+
+            StringWriter output = new StringWriter();
+
+            for (int i = 1; i <= reader.NumberOfPages; i++)
+                output.WriteLine(PdfTextExtractor.GetTextFromPage(reader, i, new SimpleTextExtractionStrategy()));
+
+            return output.ToString();
+        }
+    }
+}
