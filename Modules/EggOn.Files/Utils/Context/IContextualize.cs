c374b083fcc7a0c18cde17022bb5bd4a3a2a91e6
MarcoMauricio <marcoflip.fm@gmail.com>
2014-05-23 14:46:27 +0100

    interface para contextualizar

---
+++ Modules/EggOn.Files/Utils/Context/IContextualize.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EggOn.Files.Context
+{
+    public interface IContextualize
+    {
+        Context GetContext(String Title, String Text);
+    }
+    public class Context
+    {
+        public string Summary { get; set; }
+        public string Classification { get; set; }
+        public List<string> Entities { get; set; }
+        public string Sentiment { get; set; }
+        public string Language { get; set; }
+    }
+}
