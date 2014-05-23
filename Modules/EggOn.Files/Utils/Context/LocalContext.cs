c8feb5f3a99b24c3bba7bcd58f77d7a77b59529c
MarcoMauricio <marcoflip.fm@gmail.com>
2014-05-23 14:46:48 +0100

    Classe para implementação local

---
+++ Modules/EggOn.Files/Utils/Context/LocalContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EggOn.Files.Context
+{
+    public class LocalContext : IContextualize
+    {
+        public Context GetContext(string Title, string Text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
