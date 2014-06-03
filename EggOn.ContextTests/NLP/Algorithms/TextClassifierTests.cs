9bfcdc114e98227298b1b6374e14a0ec30a51298
MarcoMauricio <marcoflip.fm@gmail.com>
2014-06-03 15:45:36 +0100

    Criação do esqueleto de testes

---
+++ EggOn.ContextTests/NLP/Algorithms/TextClassifierTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Context.NLP.Algorithms;
+using NUnit.Framework;
+namespace Context.NLP.Algorithms.Tests
+{
+    [TestFixture()]
+    public class TextClassifierTests
+    {
+        [Test()]
+        public void ClassifyTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ EggOn.ContextTests/NLP/Services/RemoteServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Context.NLP.Services;
+using NUnit.Framework;
+namespace Context.NLP.Services.Tests
+{
+    [TestFixture()]
+    public class RemoteServiceTests
+    {
+        [Test()]
+        public void RemoteServiceTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
