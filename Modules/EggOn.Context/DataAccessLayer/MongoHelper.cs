77dba1a7f763011342fb2e53abdfc483a2e7a874
MarcoMauricio <marcoflip.fm@gmail.com>
2014-06-03 10:38:52 +0100

    Criação de Documentação

--- Modules/EggOn.Context/DataAccessLayer/MongoHelper.cs
+++ Modules/EggOn.Context/DataAccessLayer/MongoHelper.cs
@@ -1,6 +1,14 @@
 ﻿using MongoDB.Driver;
 namespace Context.DataAccessLayer
 {
+    /// <summary>
+    /// Classe auxiliar que serve para a criação de uma ligação com a base de dados. 
+    /// Todos os serviços encarregues de fazer a gestão da sua coleção na base de dados têm um 
+    /// objecto deste tipo.
+    /// </summary>
+    /// <typeparam name="T">
+    /// Tipo de Entidade a representa
+    /// </typeparam>
     public class MongoHelper<T> where T : class
     {
         public MongoCollection<T> Collection { get; private set; }
@@ -8,7 +16,6 @@ namespace Context.DataAccessLayer
         public MongoHelper()
         {
             var con = new MongoConnectionStringBuilder("EggOn");
-
             var server = MongoServer.Create(con);
             var db = server.GetDatabase(con.DatabaseName);
             Collection = db.GetCollection<T>(typeof(T).Name.ToLower());
