0d39759af2d261e879072f4b4505be6dc768aea2
MarcoMauricio <marcoflip.fm@gmail.com>
2014-06-03 10:34:42 +0100

    Implementação da interface IEggOnModule para o módulo poder ser integrado com a plataforma EggOn

---
+++ Modules/EggOn.Context/ContextModule.cs
@@ -0,0 +1,21 @@
+﻿
+
+using FlowOptions.EggOn.Logging;
+using FlowOptions.EggOn.ModuleCore;
+namespace FlowOptions.EggOn.Context
+{
+    public class ContextModule : IEggOnModule
+    {
+        public void Setup()
+        {
+            Logger.Debug("Application is registering the Context Area.");
+
+            SetupDatabase();
+        }
+
+        private void SetupDatabase()
+        {
+           // Nothing to do here
+        }
+    }
+}
