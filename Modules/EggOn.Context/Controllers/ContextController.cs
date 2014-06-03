0cb81b6af0b8190e04aa4569d2961a3bfc67a7b4
MarcoMauricio <marcoflip.fm@gmail.com>
2014-06-03 15:46:51 +0100

    Adicionada acção para criação de contexto.
    Alteração da assinatura de GetAllDocuments devido ao facto de a classe MongoCursor ter uma implementação lazy.

--- Modules/EggOn.Context/Controllers/ContextController.cs
+++ Modules/EggOn.Context/Controllers/ContextController.cs
@@ -1,7 +1,12 @@
-﻿using Context.DataAccessLayer;
+﻿using Context;
+using Context.DataAccessLayer;
+using Context.DataAccessLayer.Services;
 using Context.Models;
+using Context.NLP.Services;
 using FlowOptions.EggOn.Base.Controllers;
+using MongoDB.Bson;
 using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Web.Http;
 
@@ -11,10 +16,29 @@ namespace FlowOptions.EggOn.Context.Controllers
     {
         static DocumentService documentsHelper = new DocumentService();
         [Route("context"), HttpGet]
-        public MongoCursor<Document> GetAllDocuments()
+        public List<Document> GetAllDocuments()
         {
+            List<Document> list = new List<Document>();
             var cursor = documentsHelper.GetDocuments();
-            return cursor;
+            foreach (Document doc in cursor)
+            {
+                list.Add(doc);
+            }
+            return list;
+        }
+
+
+        [Route("context"), HttpPost]
+        public void CreateContext(string filePath, string tableName , string tableIndex)
+        {
+            ContextCore context = new ContextCore(new RemoteService(), filePath);
+            MinedObject item = context.GetContext();
+            Document doc = new Document();
+            doc.Id = ObjectId.GenerateNewId();
+            doc.TableIndex = tableIndex;
+            doc.TableName = tableName;
+
+            documentsHelper.CreateDocument(doc);
         }
     }
 }
