bee74b0b3e7b62994434bff2988211a37f46e8e2
MarcoMauricio <marcoflip.fm@gmail.com>
2014-06-05 16:32:36 +0100

    Removidos usings

--- Modules/EggOn.Context/Models/Category.cs
+++ Modules/EggOn.Context/Models/Category.cs
@@ -1,9 +1,5 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Context.Models
 {
