bd9f23b3932cab563f5a989f4921d1756976d477
MarcoMauricio <marcoflip.fm@gmail.com>
2014-06-05 16:31:53 +0100

    Alterados usings

--- Modules/EggOn.Context/NLP/Algorithms/NamedEntityExtraction.cs
+++ Modules/EggOn.Context/NLP/Algorithms/NamedEntityExtraction.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Context.NLP.Algorithms
 {
