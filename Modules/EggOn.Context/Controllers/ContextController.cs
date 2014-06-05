5701c4e3986195cb7d055a6e88e0c4760c8da807
MarcoMauricio <marcoflip.fm@gmail.com>
2014-06-05 16:32:14 +0100

    Alterado os campos estáticos

--- Modules/EggOn.Context/Controllers/ContextController.cs
+++ Modules/EggOn.Context/Controllers/ContextController.cs
@@ -1,20 +1,23 @@
 ﻿using Context;
-using Context.DataAccessLayer;
 using Context.DataAccessLayer.Services;
 using Context.Models;
 using Context.NLP.Services;
 using FlowOptions.EggOn.Base.Controllers;
 using MongoDB.Bson;
-using MongoDB.Driver;
-using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 
 namespace FlowOptions.EggOn.Context.Controllers
 {
     public class ContextController : EggOnApiController
     {
-        static DocumentService documentsHelper = new DocumentService();
+        DocumentService documentsHelper;
+        public ContextController()
+        {
+            this.documentsHelper = new DocumentService();
+        }
         [Route("context"), HttpGet]
         public List<Document> GetAllDocuments()
         {
@@ -29,16 +32,24 @@ namespace FlowOptions.EggOn.Context.Controllers
 
 
         [Route("context"), HttpPost]
-        public void CreateContext(string filePath, string tableName , string tableIndex)
+        public HttpResponseMessage CreateContext(aux cenas)
         {
-            ContextCore context = new ContextCore(new RemoteService(), filePath);
+            ContextCore context = new ContextCore(new RemoteService(), cenas.filePath);
             MinedObject item = context.GetContext();
             Document doc = new Document();
             doc.Id = ObjectId.GenerateNewId();
-            doc.TableIndex = tableIndex;
-            doc.TableName = tableName;
+            doc.TableIndex = cenas.tableIndex;
+            doc.TableName = cenas.tableName;
 
             documentsHelper.CreateDocument(doc);
+            var response = Request.CreateResponse<aux>(HttpStatusCode.Created, cenas);
+            return response;
         }
+    }
+    public class aux
+    {
+        public string filePath { get; set; }
+        public string tableName { get; set; }
+        public string tableIndex { get; set; }
     }
 }
